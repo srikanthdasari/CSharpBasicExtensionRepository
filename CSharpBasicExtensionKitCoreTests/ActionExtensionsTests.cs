c8cec811c3d2bcabe4dfe5e588afb8b03e405447
Carlos <comtechy@live.com>
2017-01-19 07:17:41 -0500

    Migrate to .NET Standard 1.1

---
+++ CSharpBasicExtensionKitCoreTests/ActionExtensionsTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using CSharpBasicExtensionsKit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpBasicExtensionKitCoreTests
+{
+    [TestClass]
+    public class ActionExtensionsTests
+    {
+        [TestMethod]
+        public void Call_WhenNull_DoNotCallAction()
+        {
+            Action action = null;
+            action.Call();
+        }
+
+        [TestMethod]
+        public void Call_WhenNotNull_DoNotCallAction()
+        {
+            var bCalled = false;
+            Action action = () => bCalled = true;
+            action.Call();
+
+            Assert.IsTrue(bCalled);
+        }
+
+        [TestMethod]
+        public void Call_WhenNotNull_WithParam()
+        {
+            var bCalled = false;
+            Action<bool> action = (x) => bCalled = x;
+            action.Call(true);
+
+            Assert.IsTrue(bCalled);
+        }
+    }
+}
